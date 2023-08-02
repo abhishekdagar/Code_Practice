ebb32b5934012228d0795c5f299ad301d5501cf4
abhishekdagar <31288082+abhishekdagar@users.noreply.github.com>
2023-08-02 14:33:26 +0530

    Add files via upload

---
+++ upper_bound.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+//https://www.codingninjas.com/studio/problems/implement-upper-bound_8165383
+//upper bound = smallest index such that array[index] > x
+//If x is not present in the array then (array size + 1) is the lower bound
+int binarySearch(vector<int> &arr , int n, int x) {
+	int low = 0;
+	int high = n - 1;
+	int ans = n;
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (arr[mid] > x)
+		{
+			ans = mid;
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1;
+		}
+
+	}
+	return ans;
+}
+int usingSTL(vector<int> &arr , int n, int x) {
+	int ub = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+	return ub;
+}
+int main() {
+	int n;
+	cin >> n;
+	int x;
+	cin >> x;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	int ans = binarySearch(arr, n, x);
+	//int ans = usingSTL(arr, n, x);
+	cout << ans;
+	return 0;
+}
+//TC is O(logN)
