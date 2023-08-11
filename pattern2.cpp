f16ddd62c7a7dd9b440ac13c16331f33c7556d28
abhishekdagar <31288082+abhishekdagar@users.noreply.github.com>
2023-08-11 17:11:13 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+//https://practice.geeksforgeeks.org/problems/right-triangle/1
+/*
+	*
+	* *
+	* * *
+	* * * *
+	* * * * *
+*/
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
